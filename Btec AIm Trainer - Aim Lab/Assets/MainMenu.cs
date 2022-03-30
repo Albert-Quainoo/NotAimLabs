26063b1704ab129972518fcfb1e024046d9f04d5
Vowssss <92716684+Vowssss@users.noreply.github.com>
2022-03-30 11:10:42 +0000

    Update MainMenu.cs

--- Btec AIm Trainer - Aim Lab/Assets/MainMenu.cs
+++ Btec AIm Trainer - Aim Lab/Assets/MainMenu.cs
@@ -6,8 +6,10 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
+
     {
         SceneManager.LoadScene("SampleScene");
+
       }
 
     public void QuitGame()
